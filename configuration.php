5068cb8ea7b524286b70218c13870a4cff4c99db
Andriah91 <arasolofomamp@gmail.com>
2016-07-25 17:00:40 +0300

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -8,16 +8,16 @@
 
 return array(
     'sandbox' => array(
-        'devId' => '2e953e42-793a-4323-9456-2f4e84c5c79a',
-        'appId' => 'MCCfa6f66-2ab3-4006-ad0b-70928b327c7',
-        'certId' => 'b2c7356d-0f90-4c18-8c52-97199f233be3',
-        'userToken' => 'AgAAAA**AQAAAA**aAAAAA**LkfUVQ**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4GhDpCDpA2dj6x9nY+seQ**f4oDAA**AAMAAA**WJ5HtnkaaDMdMVA/pT/hqesdPnsYfc7gJHyZv4Biv5w/+IsdusRDr00ajZIJz9ziC8Pr89Y5BPYo2L0HjZbHnDJAaTNoQRCJTwh4+ahPG/uAABJYLQKDc6kDSMheFrhhSe5CL6XcFAXo/PvvrP2T4ktYTURImByHE0fiMtGrcw+xj+aqOBWldoaKw8MU3DoquRrwvvLjxPtcIOgZoJ5bygnSY7/w5ue6X+fAkjWOp6QOhfvJu29/Ukab/0+R8E85pl1SMmV4kLv+JlgE2OEoX3Kh5AMd4oSSjsmLm5NQY1mYNk7rY5/1BZ/ewqA4TG8v8W5ZKnuteoXVMt1360uSvd8YhN/NxRFYI+/+X10rXeyHdLrFl8r0sE+P+HtNqP3wd6fXfC+JyJC+rwmHu6T5EVqzVDZ9r71B5o3+CT5t+9IMVLUw4k7UE0zowcBjLuCxen2mfYvB6WE0vA2mzgmKtkerO3b4cL18Sl3kIu953FK9TDFG6kUWbuy83jW3CzZCM4YZPEs4pwqZ0okz3TpLi2jmREONFzhYKcWjef9n+fWvOELORACpVqGbDow9FaAWqthxiz14exGDXFW6LukAqpEHvzw49X7BztIBLo/IoGMMq0Qru9pBR9hznsiYmUaC2pRfg16m9THRPqxHS0FPi09dV5mtOC/m+7L4UG9xyk//N58Zt5bioWTxuO17wysqjlngQct+1VhyL61Onxc9u+rJEHD35iU2RnEfhZX1vmxUR5keoGDl2hkc4bcZDIkk'
+        'devId' => 'DEV_ID',
+        'appId' => 'APP_ID',
+        'certId' => 'CERT_ID',
+        'userToken' => 'USER_TOKEN'
     ),
     'production' => array(
-        'devId' => '2e953e42-793a-4323-9456-2f4e84c5c79a',
-        'appId' => 'MCC8f6a3c-940b-46bc-b172-d4809d46076',
-        'certId' => 'd8c64b10-d200-42bc-9c3b-6ed55017a0a0',
-        'userToken' => 'AgAAAA**AQAAAA**aAAAAA**OUPUVQ**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wJmIanDZCAow+dj6x9nY+seQ**SfYCAA**AAMAAA**YJ+6ItGQ0I5sxZdZpRrxz+baThwNO8eRsd2TWwcCs5ajrNZwx7u9KAIWVfvJsijJZ65TYWLA5ZXp+xmp/Dr78sY3Sht6DpAoMzgCZ3NYTwzENdUlBVKqKufO6lSkIyObMp+Wh0+yKq28ELqAEu/VFLWrHQoy0gOoMLrHUzKLwPxQp0OTKg7KOqht9qdzvK1uCIro+QDJkrPlMvdyzhQOXVsImZTBpDxPB3saIV5ChvfxX4TT03pQhoA3eLZcs5iT+K1bueSbUr3KJAIhjJQy+kiuTrv50/BS2pMplDvXX+cdcIEkeoeHCS7NPIdv5nyt5OtziwjPn50Og77M1bxfD8GR4zHOSQzOLP32DVJrOqDLF3yHWpo6BdOTj77R26E4GKfwSgWIE9vUiNAkRD0TMLiJjSPBHqTZZ3voHnZQVSoezpiYf46gxlLMdzOYDQDtD69wZ4nlPIkV4UuEWMr6RkeGqbM/vrFUPlopcmgcvrTcTkK/pGx7h0th9OYZ3VT2bYTLHRLmQtHzsILr3+G7aFye8XbTDYKYlwmhb4DeobuK2UVKUvPYDWdbtOThlGGIa/b9GDkiCF9FSwDVyYDelWiveWHpkqexrJqtTqFo+VEa0eLvULDfad6VyyHDIKA2/Iew8LcbyxjokEt0zUV6mOST8kEMGZ83fjsnvFxhd2ngDyXTmD6f67YvQn28IW9JN2VecLjNngfSQNmXbUjEWfjzvnH9Z4lywJp0q4ZVXfY4InIE5DqUGsPy1VY6BHf0'
+        'devId' => 'DEV_ID',
+        'appId' => 'APP_ID',
+        'certId' => 'CERT_ID',
+        'userToken' => 'USER_TOKEN'
     ),
     'findingApiVersion' => '1.12.0',
     'tradingApiVersion' => '947',
